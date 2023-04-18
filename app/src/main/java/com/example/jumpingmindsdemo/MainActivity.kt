50750af788bc1dd0c0675784f465dccaa5e7dca4
Nishant Bansal <bansal.nishant18052000@gmail.com>
2023-04-18 13:08:36 +0530

    Splash Screen added

--- app/src/main/java/com/example/jumpingmindsdemo/MainActivity.kt
+++ app/src/main/java/com/example/jumpingmindsdemo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.jumpingmindsdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.jumpingmindsdemo.views.ListingScreen
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.beginTransaction().apply {
